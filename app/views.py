17fc1dca20ef6ef27121ee7fdf9df9b25c192dfe
marcia <marcia@hotmail.com>
2019-07-10 13:26:54 -0300

    Fazer importação correta

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from .models import Empresas
+from .models import Empresas, Usuaria
+from app.forms import UsuariaForm, LoginForm
+
 # from app.forms import UsuariaForm
 
 # Create your views here.
@@ -9,28 +11,38 @@ def mostrar_index(request):
 
 
 def mostrar_cadastro(request):
-    # formulario = UsuariaForm (request.POST or None)
-    # msg = ''
+    formulario = UsuariaForm(request.POST or None)
+    msg = ''
 
-    # if formulario.is_valid():
-    #     formulario.save()
-    #     formulario = UsuariaForm()
-    #     msg = 'Cadastro realizado com sucesso linda!!!!!!!'
+    if formulario.is_valid():
+        formulario.save()
+        formulario = UsuariaForm()
+        msg = 'Cadastro realizado com sucesso linda!!!!!!!'
 
-    # contexto = {
-    #     'form': formulario,
-    #     'msg':msg
-    # }
-    return render (request, 'cadastro2.html',)  
+    contexto = {
+        'form': formulario,
+        'msg':msg
+    }
+    return render (request, 'cadastro2.html', contexto)  
 
 def mostrar_login(request):
-    return render (request, 'login3.html')
+    formulario = LoginForm(request.POST or None)
+    msg = ''
+
+    if formulario.is_valid():
+        formulario.save()
+        formulario = UsuariaForm()
+        msg = 'Cadastro realizado com sucesso linda!!!!!!!'
+
+    contexto = {
+        'form': formulario,
+        'msg':msg
+    }
+    return render (request, 'login3.html', contexto)
 
 def mostrar_areas(request):
     return render (request, 'areas4.html')
 
-def mostrar_vagas(request):
-    
-    empresa = Empresas.objects.all()
-    return render (request,'vagas5.html', {'empresa': empresa})   
+def mostrar_vagas(request):    
+    return render (request,'vagas5.html',)   
 
