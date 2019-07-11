20e72b4f2a691cf51cd97f6c874ac11721724601
marcia <marcia@htomail.com>
2019-07-11 17:43:38 -0300

    Criar JS

--- app/static/JS/scripts2.js
+++
@@ -1,9 +0,0 @@
-const button= document.querySelector('#botao');
-
-function abrirLogin(){
-    button.classList.toggle('ativo');
-    window.location.pathname = '/login3';
-
-}
-
-button.onclick= abrirLogin;
