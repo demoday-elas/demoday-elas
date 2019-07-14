41ac185aa51a536fa908d1ccd5c48593ee7eb73f
Ketlyn Ferreguete <ketlynferreguete@outlook.com>
2019-07-14 12:30:58 -0300

    estilização da pagina de login

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,12 +1,10 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 
-// botões de voltar/avançar
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// thumbnail
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
@@ -15,15 +13,15 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1} 
+  if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
+      slides[i].style.display = "none";  
   }
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block"; 
+  slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
 
